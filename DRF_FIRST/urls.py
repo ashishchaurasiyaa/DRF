ebf48f8068546ffedfe0497d25492905c03441b0
Ashish <AChaurasiya@youngman.co.in>
2024-02-05 11:47:55 +0530

    serilization

--- DRF_FIRST/urls.py
+++ DRF_FIRST/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('studentapi/<int:pk>', views.student_data,name='student'),
+    path('query_set/', views.student_list,name='queryset'),
 ]
